4cff3d3549e280005af3b8460d52fcdf7669d5b9
Clems-web <clement.servais1@gmail.com>
2020-12-01 10:59:36 +0100

    finito

--- exo 3/exercice3.js
+++ exo 3/exercice3.js
@@ -1,7 +1,7 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let i;
 
-for( var i =35 ;  > ; )
-{
+for( i = 35 ; i > 25 ; i-- ) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
 }
 
--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,7 +1,7 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let i;
 
-for(var i=2;   ; )
-{
+for(i = 2; i < 12 ; i+=2) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
 }
 
